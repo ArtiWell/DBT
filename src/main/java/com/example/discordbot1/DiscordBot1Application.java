dda09483e8235b3861cfe9276d74fca49f8c3d79
TAO <psydak94@gmail.com>
2024-02-01 17:41:36 +0300

    commit

--- src/main/java/com/example/discordbot1/DiscordBot1Application.java
+++ src/main/java/com/example/discordbot1/DiscordBot1Application.java
@@ -15,7 +15,7 @@ public class DiscordBot1Application {
     public static void main(String[] args) {
 		ArtemBot artemBot = new ArtemBot();
 
-        JDABuilder.createLight("MTE5ODU3MTU3NjI2MjY3MjQzNA.GhZvwz.Ux3-YFnoRA7as5hCMDTE2HOnxEgK6JK2HUq2mk11")
+        JDABuilder.createLight("MTE5ODU3MTU3NjI2MjY3MjQzNA.GhZvwz.Ux3-YFnoRA7as5hCMDTE2HOnxEgK6JK2HUq2mk")
                 .addEventListeners(artemBot)
                 .enableIntents(
                         List.of(
@@ -24,7 +24,7 @@ public class DiscordBot1Application {
                         )
                 )
                 .setStatus(OnlineStatus.ONLINE)
-				.setActivity(Activity.watching("Pornt"))
+				.setActivity(Activity.watching("Porno"))
                 .build();
 
 
--- src/main/java/com/example/discordbot1/listeners/FlipListener.java
+++ src/main/java/com/example/discordbot1/listeners/FlipListener.java
@@ -1,12 +1,9 @@
-package com.example.discordbot1;
+package com.example.discordbot1.listeners;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-
-public class ArtemBot extends ListenerAdapter {
-
-
+public class FlipListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()){
