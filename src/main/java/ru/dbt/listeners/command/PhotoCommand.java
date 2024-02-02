f128241904f68d7a1494d100e6aaefb429361db8
Boeing <pm.to.dmitry@gmail.com>
2024-02-02 17:24:30 +0300

    DBT-3 сделал картинку в картинке, спроси меня как

---
+++ src/main/java/ru/dbt/listeners/command/PhotoCommand.java
@@ -0,0 +1,49 @@
+package ru.dbt.listeners.command;
+
+import lombok.SneakyThrows;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.utils.FileUpload;
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+@Component
+public class PhotoCommand implements Command {
+    @Override
+    @SneakyThrows
+    public void run(MessageReceivedEvent event) {
+        String imagePath = "src/main/resources/static/php.jpg";
+        String imagePath1 = "src/main/resources/static/js.png";
+        File file1 = new File(imagePath);
+        File file2 = new File(imagePath1);
+
+        BufferedImage img1 = ImageIO.read(file1);
+        BufferedImage img2 = ImageIO.read(file2);
+        BufferedImage im = new BufferedImage(1024,1024,BufferedImage.TYPE_INT_ARGB);
+        im.getGraphics().drawImage(img1,0,0,null);
+        im.getGraphics().drawImage(img2,0,0,null);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(im, "png", bos );
+        byte [] data = bos.toByteArray();
+
+        FileUpload fileUpload = FileUpload.fromData(data,"poistenepizdec.png");
+
+            event.getChannel().sendFiles(fileUpload).queue();
+
+
+    }
+
+    @Override
+    public String getKey() {
+        return "photo";
+    }
+}
