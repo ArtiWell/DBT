1461c602799daaba58f0f6f268c18ff0b308498e
TAO <psydak94@gmail.com>
2024-02-05 20:31:00 +0300

    commit

--- src/main/java/ru/dbt/listeners/command/casino/CasinoCommand.java
+++ src/main/java/ru/dbt/listeners/command/casino/CasinoCommand.java
@@ -1,6 +1,5 @@
 package ru.dbt.listeners.command.casino;
 
-import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.utils.FileUpload;
@@ -11,42 +10,51 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
-@AllArgsConstructor
 public class CasinoCommand implements Command {
 
+    public CasinoCommand(RandomField randomField) {
+        this.randomField = randomField;
+    }
+
     private final RandomField randomField;
+    private final Map<Integer, BufferedImage> mapImg = loadPicture();
+
+
     @Override
     public void run(MessageReceivedEvent event) {
 
-        event.getChannel().sendFiles(printToScreen(randomField.randomArray(3,3))).queue();
+        event.getChannel().sendFiles(printToScreen(randomField.randomArray(3, 3))).queue();
     }
 
+    @SneakyThrows
+    private Map<Integer, BufferedImage> loadPicture() {
+        Map<Integer, BufferedImage> map = new HashMap<>();
+        map.put(0 ,ImageIO.read(new File("src/main/resources/static/black Fon.jpg")));
+        map.put(1, ImageIO.read(new File("src/main/resources/static/1111.png")));
+        map.put(2, ImageIO.read(new File("src/main/resources/static/111.png")));
+        map.put(3, ImageIO.read(new File("src/main/resources/static/333.png")));
+        map.put(4, ImageIO.read(new File("src/main/resources/static/444.png")));
+        map.put(5, ImageIO.read(new File("src/main/resources/static/55.png")));
+
+       return map;
+    }
 
 
     @SneakyThrows
     private FileUpload printToScreen(int[][] array) {
-        BufferedImage imgFon = ImageIO.read(new File("src/main/resources/static/js.png"));
-
-        List<BufferedImage> list = new ArrayList<>();
-        list.add(ImageIO.read(new File("src/main/resources/static/1.png")));
-        list.add(ImageIO.read(new File("src/main/resources/static/2.png")));
-        list.add(ImageIO.read(new File("src/main/resources/static/3.png")));
-        list.add(ImageIO.read(new File("src/main/resources/static/4.png")));
-        list.add(ImageIO.read(new File("src/main/resources/static/5.png")));
-
         BufferedImage im = new BufferedImage(array.length * 200 + 10, array[1].length * 200 + 10, BufferedImage.TYPE_INT_ARGB);
 
-        im.getGraphics().drawImage(imgFon, 0, 0, null);
+        im.getGraphics().drawImage(mapImg.get(0), 0, 0, null);
 
-        for (int i = 0, a = 10, b = 10; i < array.length; i++, a += 200) {
-            for (int j = 0; j < array[i].length; j++, b += 200) {
-                im.getGraphics().drawImage(list.get(array[i][j]), a, b, null);
+        for (int i = 0, xBorder = 10; i < array.length; i++, xBorder += 200) {
+            for (int j = 0,yBorder = 10; j < array[i].length; j++, yBorder += 200) {
+
+                im.getGraphics().drawImage(mapImg.get(array[i][j]), xBorder, yBorder, null);
             }
-            b = 10;
         }
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
